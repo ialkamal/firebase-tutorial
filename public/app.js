bb13677bc8074fbcc2a0823eb131bea796f5a7a3
Ismail Al Kamal <ismail@nawatt.com>
2020-11-26 04:47:14 +0300

    finished firebase tutorial

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
 const auth = firebase.auth();
+const db = firebase.firestore();
 
 const whenSignedIn = document.getElementById("whenSignedIn");
 const whenSignedOut = document.getElementById("whenSignedOut");
 const signInBtn = document.getElementById("signInBtn");
 const signOutBtn = document.getElementById("signOutBtn");
 const userDetails = document.getElementById("userDetails");
+const createThing = document.getElementById("createThing");
+const thingsList = document.getElementById("thingsList");
+
+let thingsRef;
+let unsubscribe;
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -19,9 +25,28 @@ auth.onAuthStateChanged((user) => {
     whenSignedIn.hidden = false;
     whenSignedOut.hidden = true;
     userDetails.textContent = JSON.stringify(user, null, 2);
+    thingsRef = db.collection("things");
+    createThing.onclick = () => {
+      const { serverTimestamp } = firebase.firestore.FieldValue;
+      thingsRef.add({
+        uid: user.uid,
+        name: faker.commerce.productName(),
+        createdAt: serverTimestamp(),
+      });
+
+      unsubscribe = thingsRef
+        .where("uid", "==", user.uid)
+        .onSnapshot((querySnapshot) => {
+          const items = querySnapshot.docs.map((doc) => {
+            return `<li>${doc.data().name}</li>`;
+          });
+          thingsList.innerHTML = items.join("");
+        });
+    };
   } else {
     whenSignedIn.hidden = true;
     whenSignedOut.hidden = false;
+    unsubscribe && unsubscribe();
   }
 });
 
